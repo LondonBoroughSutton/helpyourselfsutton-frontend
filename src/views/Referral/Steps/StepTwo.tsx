64ad50a06581fdd4d9aacfa13faa33d91b3b4c1a
seanjessen <info@seanjessen.co.uk>
2022-01-31 10:29:23 +0000

    Referral step two styling and layout

--- src/views/Referral/Steps/StepTwo.tsx
+++ src/views/Referral/Steps/StepTwo.tsx
@@ -26,8 +26,7 @@ interface IButtonProps {
 const ReferButton: React.FunctionComponent<IButtonProps> = observer(
   ({ icon, text, setWho, active, subText, smallPadding, autoFocus }) => (
     <button
-      className={cx(
-        'flex-container flex-container--mobile-no-padding flex-container--justify referral__button flex-container--align-center',
+      className={cx('referral__button',
         {
           'referral__button--active': active,
         }
@@ -36,23 +35,11 @@ const ReferButton: React.FunctionComponent<IButtonProps> = observer(
       onClick={() => setWho(text)}
       autoFocus={autoFocus}
     >
-      <div
-        className={cx('flex-col flex-col--12 flex-col--mobile--2 flex-col--tablet-large--2', {
-          'referral__button--small-margin': smallPadding,
-        })}
-      >
-        <FontAwesomeIcon icon={icon} />
-      </div>
-      <div
-        className={cx('flex-col flex-col--12 flex-col--mobile--10 flex-col--tablet-large--10', {
-          'referral__button--small-margin': smallPadding,
-        })}
-      >
-        <h4>{text}</h4>
-      </div>
-      <div className="flex-col flex-col--10 mobile-hide tablet-hide tablet--large-hide">
-        <p className="body--s referral__button--subtext">{subText}</p>
-      </div>
+      <FontAwesomeIcon
+        icon={icon}
+        className="referral__button__icon" />
+      <h4 className="referral__button__title">{text}</h4>
+      <p className="referral__button__description">{subText}</p>
     </button>
   )
 );
@@ -63,47 +50,37 @@ const StepTwo: React.FunctionComponent<IProps> = ({ name, referralStore }) => {
   }
 
   return (
-    <div className="flex-col flex-col--7 flex-col--mobile--12 flex-col--tablet--12 flex-col--tablet-large--6">
-      <div className="flex-container flex-container--mobile-no-padding referral--intro--no-padding referral__step-container referral__form">
-        <div className="flex-col flex-col--12 flex-col--mobile--12">
-          <p className="referral__step-container--question body--s">{`Who would like to be connected to ${name}?`}</p>
-        </div>
-        <div className="flex-col flex-col--12 flex-col--mobile--12">
-          <div className="flex-container referral__step-container--inner referral--intro--no-padding">
-            <div className="flex-col flex-col--4 flex-col--mobile--12 flex-col--tablet-large--12">
-              <ReferButton
-                icon="user"
-                text="Myself"
-                setWho={referralStore.setWhoFor}
-                active={referralStore.whoFor === 'Myself'}
-                subText="Filling in my own details"
-                autoFocus={true}
-              />
-            </div>
-            <div className="flex-col flex-col--4 flex-col--mobile--12 flex-col--tablet-large--12">
-              <ReferButton
-                icon="user-friends"
-                text="A friend or family member"
-                setWho={referralStore.setWhoFor}
-                active={referralStore.whoFor === 'A friend or family member'}
-                subText="Someone I know filling in my details"
-                smallPadding={true}
-              />
-            </div>
-            <div className="flex-col flex-col--4 flex-col--mobile--12 flex-col--tablet-large--12">
-              <ReferButton
-                icon="hands-helping"
-                text="Someone else"
-                setWho={referralStore.setWhoFor}
-                active={referralStore.whoFor === 'Someone else'}
-                subText="A client, customer, or member of the public"
-              />
-            </div>
-          </div>
+    <div>
+      <div className="referral__step-container">
+        <h2 className="referral__step-container__question">{`Who would like to be connected to ${name}?`}</h2>
+        <div className="referral__step-container__answers">
+          <ReferButton
+            icon="user"
+            text="Myself"
+            setWho={referralStore.setWhoFor}
+            active={referralStore.whoFor === 'Myself'}
+            subText="Filling in my own details"
+            autoFocus={true}
+          />
+          <ReferButton
+            icon="user-friends"
+            text="A friend or family member"
+            setWho={referralStore.setWhoFor}
+            active={referralStore.whoFor === 'A friend or family member'}
+            subText="Someone I know filling in my details"
+            smallPadding={true}
+          />
+          <ReferButton
+            icon="hands-helping"
+            text="Someone else"
+            setWho={referralStore.setWhoFor}
+            active={referralStore.whoFor === 'Someone else'}
+            subText="A client, customer, or member of the public"
+          />
         </div>
       </div>
-      <section className="referral__actions">
-        <div className="flex-container flex-container--column flex-container--align-start">
+      <div className="referral__actions">
+        <div className="flex-container flex-container--no-padding flex-container--column flex-container--align-start">
           <Button
             text="Continue"
             type="submit"
@@ -115,7 +92,7 @@ const StepTwo: React.FunctionComponent<IProps> = ({ name, referralStore }) => {
             dangerouslySetInnerHTML={{__html: html(referralStore.stepDescription) }}
             className="body--s" />
         </div>
-      </section>
+      </div>
     </div>
   );
 };
