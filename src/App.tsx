ff95e939d06939fea111d91688c71f79b60771b5
seanjessen <info@seanjessen.co.uk>
2022-02-01 12:51:07 +0000

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
