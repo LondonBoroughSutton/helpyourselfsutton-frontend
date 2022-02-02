47c51a1ae25982110f4fea33d7ba054db440d0f2
seanjessen <info@seanjessen.co.uk>
2022-02-02 17:19:45 +0000

    Amended button image styling

--- src/components/Button/ButtonLink.tsx
+++ src/components/Button/ButtonLink.tsx
@@ -9,12 +9,13 @@ import './Button.scss';
 interface IProps {
   text: string;
   icon?: IconName;
+  image?: string;
   href: string;
   target?: string;
   category?: boolean;
 }
 
-const ButtonLink: React.FunctionComponent<IProps> = ({ text, icon, href, target = '_self', category }) => (
+const ButtonLink: React.FunctionComponent<IProps> = ({ text, icon, image, href, target = '_self', category }) => (
   <Link
     className={cx('button', 'button__link', {
       'button__category': category,
@@ -22,6 +23,7 @@ const ButtonLink: React.FunctionComponent<IProps> = ({ text, icon, href, target 
     to={href}
     target={target}
     rel={target === '_blank' ? 'noopener nofollow noreferrer' : undefined}>
+    {image && <img src={image} alt={text} className="button__image" />}
     <span>{text}</span>
     {icon && <FontAwesomeIcon icon={icon} className={cx('button__icon')} />}
   </Link>
--- src/components/LandingPages/LandingPages.tsx
+++ src/components/LandingPages/LandingPages.tsx
@@ -6,11 +6,7 @@ import ButtonLink from '../Button/ButtonLink';
 import './LandingPages.scss';
 
 import PageStore from '../../stores/pageStore';
-
-interface IContent {
-  id: string;
-  title: string;
-}
+import { IPage } from '../../types/types';
 
 interface IProps {
   pageStore?: PageStore;
@@ -35,11 +31,12 @@ class LandingPages extends Component<IProps> {
         <div className="flex-container">
           <h2 className="landing-pages__heading h4">Browse area of interest</h2>
           <div className="landing-pages__items">
-            {pageStore.pages?.map((page: IContent) => {
+            {pageStore.pages?.map((page: IPage) => {
               return (
                 <ButtonLink
                   href={'/' + page.id}
                   text={page.title}
+                  image={page.image}
                   key={page.id}
                   category={true}
                 />
