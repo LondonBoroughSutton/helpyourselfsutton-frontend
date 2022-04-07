53f9c175a08127c5a66015e29fe5832117c9f769
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-04-07 15:02:08 +0100

    Updated deploy scripts

--- .cloudfoundry/store.sh
+++ .cloudfoundry/store.sh
@@ -3,13 +3,23 @@
 # ================================
 # Stores an object in the AWS S3 Secrets Bucket
 # This script will install the AWS CLI and the CF CLI
-# If you don't want these on your system, install using the docker helper script:
-# ./develop bash cloudfoundry/store_env.sh
+# If you don't want these on your system, install using the docker helper script.
+# First, if you have an environment file, set the environment variables:
+# source .cloudfoundry/environment.[environment]
+# export CF_USERNAME CF_PASSWORD CF_ORGANISATION CF_SPACE CF_ENV_SERVICE CF_ENV_SERVICE_KEY
+# Then run the helper script:
+# ./develop store
 # ================================
 
 # Requires the following environment variables:
-# CF_SECRET_SERVICE
-# CF_SECRET_SERVICE_KEY
+# $CF_ENV_SERVICE = The name of the S3 bucket to store the files
+# $CF_ENV_SERVICE_KEY = The name of the service key that holds the access credentials
+
+# Can accept the following environment variables
+# $CF_USERNAME = The Cloud Foundry username.
+# $CF_PASSWORD = The Cloud Foundry password.
+# $CF_ORGANISATION = The Cloud Foundry organisation.
+# $CF_SPACE = The Cloud Foundry space.
 
 # Bail out on first error.
 set -e
@@ -22,19 +32,31 @@ BLUE='\e[1;34m'
 GREEN='\e[1;32m'
 ENDCOLOUR='\e[1;m'
 
-if [ -z "$CF_SECRET_SERVICE" ] || [ -z "$CF_SECRET_SERVICE_KEY" ]; then
-    echo -e "${RED}Missing variables for cf service-key${ENDCOLOUR}"
-    exit
+# Get the Cloud Foundry details
+if [ -z "$CF_USERNAME" ]; then
+    read -p 'Cloudfoundry Username: ' CF_USERNAME
 fi
 
-# Get the Cloud Foundry details
-read -p 'Cloudfoundry Username: ' CF_USERNAME
+if [ -z "$CF_PASSWORD" ]; then
+    read -sp 'Cloudfoundry Password: ' CF_PASSWORD
+    echo
+fi
 
-read -sp 'Cloudfoundry Password: ' CF_PASSWORD
-echo
-read -p 'Cloudfoundry Organisation: ' CF_ORGANISATION
+if [ -z "$CF_ORGANISATION" ]; then
+    read -p 'Cloudfoundry Organisation: ' CF_ORGANISATION
+fi
 
-read -p 'Cloudfoundry Space: ' CF_SPACE
+if [ -z "$CF_SPACE" ]; then
+    read -p 'Cloudfoundry Space: ' CF_SPACE
+fi
+
+if [ -z "$CF_ENV_SERVICE" ]; then
+    read -p 'AWS S3 Bucket name: ' CF_ENV_SERVICE
+fi
+
+if [ -z "$CF_ENV_SERVICE_KEY" ]; then
+    read -p 'AWS service key for the S3 Bucket: ' CF_ENV_SERVICE_KEY
+fi
 
 # Install required packages
 apt-get update && apt-get install -y --allow-unauthenticated wget jq unzip less
@@ -55,41 +77,11 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update && apt-get install -y --allow-unauthenticated cf7-cli
 
-# Get the upload details
-read -p 'Which environment is to be updated? (staging or production): ' ENVIRONMENT
-
-if [ "$ENVIRONMENT" != 'staging' ] && [ "$ENVIRONMENT" != 'production' ]; then
-    echo -e "${RED}The environment should be one of staging or production${ENDCOLOUR}"
-    exit
-fi
-
-echo 'What is the path to the new environment file? relative to the application root (e.g. .env)'
-
-read FILE_PATH
-
-if [ ! -e "$APPROOT/$FILE_PATH" ]
-then
-    echo -e "${RED}The environment file does not exist${ENDCOLOUR}"
-    exit
-fi
-
-ENV_SECRET_FILE=".env.frontend.${ENVIRONMENT}"
-
-# Check the user is happy to store the proposed update
-read -p "Storing $FILE_PATH as $ENV_SECRET_FILE Proceed? (Y/n): " PROCEED
-
-PROCEED=${PROCEED:-'Y'}
-
-if [ "$PROCEED" != 'Y'] && [ "$PROCEED" != 'y' ]; then
-    echo -e "${RED}Aborting file storage${ENDCOLOUR}"
-    exit
-fi
-
 # Login to Cloud Foundry.
 cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANISATION -s $CF_SPACE
 
 # Get the .env file from the secret S3 bucket
-cf service-key $CF_SECRET_SERVICE $CF_SECRET_SERVICE_KEY | sed -n '/{/,/}/p' | jq . > secret_access.json
+cf service-key $CF_ENV_SERVICE $CF_ENV_SERVICE_KEY | sed -n '/{/,/}/p' | jq . > secret_access.json
 
 # Export the AWS S3 access credentials for use by the AWS CLI
 export AWS_ACCESS_KEY_ID=`jq -r .aws_access_key_id secret_access.json`
@@ -100,9 +92,89 @@ export AWS_DEFAULT_OUTPUT=json
 
 rm secret_access.json
 
-echo -e "${GREEN}Uploading $APPROOT/$FILE_PATH to bucket $AWS_BUCKET_NAME as object $ENV_SECRET_FILE${ENDCOLOUR}"
+# Select what operation to perform
+read -p '(L)ist, (G)et, (P)ut or (D)elete an object: ' ACTION
+case $ACTION in
+    "L"|"l"|"G"|"g"|"P"|"p"|"D"|"d")
+    ;;
+    *)
+    echo -e "${RED}The action should be one of (L)ist, (G)et, (P)ut or (D)elete${ENDCOLOUR}"
+    exit
+    ;;
+esac
 
-aws s3api put-object --bucket ${AWS_BUCKET_NAME} --key "$ENV_SECRET_FILE" --body "$APPROOT/$FILE_PATH"
+if [ "$ACTION" == 'L' ] || [ "$ACTION" == 'l' ]; then
+# List the bucket contents
+    echo -e "${GREEN}The contents of bucket $AWS_BUCKET_NAME are:${ENDCOLOUR}"
+    echo `aws s3api list-objects --bucket ${AWS_BUCKET_NAME}`
+fi
+
+if [ "$ACTION" == 'G' ] || [ "$ACTION" == 'g' ]; then
+    # Download a bucket object
+    read -p 'What is the key of the object to download?' OBJECT_KEY
+    echo "Downloading $OBJECT_KEY from bucket $AWS_BUCKET_NAME to ${PWD}/${OBJECT_KEY}"
+    aws s3api get-object --bucket ${AWS_BUCKET_NAME} --key ${OBJECT_KEY} ${PWD}/${OBJECT_KEY}
+fi
+
+if [ "$ACTION" == 'P' ] || [ "$ACTION" == 'p' ]; then
+    # Get the upload details
+    read -p 'Which environment is to be updated? (staging or production): ' ENVIRONMENT
+
+    if [ "$ENVIRONMENT" != 'staging' ] && [ "$ENVIRONMENT" != 'production' ]; then
+        echo -e "${RED}The environment should be one of staging or production${ENDCOLOUR}"
+        exit
+    fi
+
+    echo 'What is the path to the file? relative to the application root (e.g. .env, storage/cloud/files/public/...)'
+
+    read FILE_PATH
+
+    if [ ! -e "$APPROOT/$FILE_PATH" ]; then
+        echo -e "${RED}The file does not exist${ENDCOLOUR}"
+        exit
+    fi
+
+    if [[ $FILE_PATH == *".env"* ]]; then
+        FILE_KEY=".env.frontend.${ENVIRONMENT}"
+    else
+        read -p 'What is the path this file should be stored as? (e.g. files/public/abc123.png): ' FILE_KEY
+    fi
+
+    if [ -z "$FILE_KEY" ]; then
+        echo -e "${RED}The file does not match the type of file this script is for${ENDCOLOUR}"
+        exit
+    fi
+
+    # Check the user is happy to store the proposed update
+    read -p "Storing $FILE_PATH as $FILE_KEY Proceed? (Y/n): " PROCEED
+
+    PROCEED=${PROCEED:-'Y'}
+
+    if [ "$PROCEED" != 'Y' ] && [ "$PROCEED" != 'y' ]; then
+        echo -e "${RED}Aborting file storage${ENDCOLOUR}"
+        exit
+    fi
+
+    echo "Uploading $APPROOT/$FILE_PATH to bucket $AWS_BUCKET_NAME as object $FILE_KEY"
+
+    aws s3api put-object --bucket ${AWS_BUCKET_NAME} --key "$FILE_KEY" --body "$APPROOT/$FILE_PATH"
+
+fi
+
+if [ "$ACTION" == 'D' ] || [ "$ACTION" == 'd' ]; then
+    # Delete a bucket object
+    read -p 'What is the key of the object to delete: ' OBJECT_KEY
+    # Check the user is happy to delete the object
+    read -p "Deleting $OBJECT_KEY from bucket $AWS_BUCKET_NAME Proceed? (Y/n): " PROCEED
+
+    PROCEED=${PROCEED:-'Y'}
+
+    if [ "$PROCEED" != 'Y' ] && [ "$PROCEED" != 'y' ]; then
+        echo -e "${RED}Aborting object delete${ENDCOLOUR}"
+        exit
+    fi
+    aws s3api delete-object --bucket ${AWS_BUCKET_NAME} --key ${OBJECT_KEY}
+fi
 
 # Remove the AWS client
 rm -Rf ${PWD}/aws
