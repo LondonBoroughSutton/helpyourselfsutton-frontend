5618e00511f46663f3cffff1e8af31e4b75a85f7
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-06-24 17:15:10 +0100

    Changed notice on banner to read public beta

--- src/components/BetaBanner/BetaBanner.tsx
+++ src/components/BetaBanner/BetaBanner.tsx
@@ -6,7 +6,7 @@ import './BetaBanner.scss';
 const BetaBanner: React.FunctionComponent = () => {
   return (
     <div className="beta-banner">
-      <p>The site is in Private Beta and if you find a issue or bug please{' '}
+      <p>The site is in Public Beta and if you find a issue or bug please{' '}
         <a
           href={`https://docs.google.com/forms/d/e/1FAIpQLSfEdZWKC0kz1ht6qLRO1SGDWAmK2HzvAl7O-SxPzN9f7pzBOg/viewform`}
           target="_blank"
