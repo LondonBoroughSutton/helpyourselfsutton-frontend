2aad4b195f84e3440944d7e0f576a9d326b24d03
Haider Ismail <haider.i@icloud.com>
2023-04-18 20:45:22 +0100

    updated BETA banner

--- src/components/BetaBanner/BetaBanner.tsx
+++ src/components/BetaBanner/BetaBanner.tsx
@@ -6,15 +6,14 @@ const BetaBanner: React.FunctionComponent = () => {
   return (
     <div className="beta-banner">
       <p>
-        The site is in Public Beta.{' '}
+        The site is in Beta. We would really like to hear your views,{' '}
         <a
           href={`https://docs.google.com/forms/d/e/1FAIpQLSfEdZWKC0kz1ht6qLRO1SGDWAmK2HzvAl7O-SxPzN9f7pzBOg/viewform`}
           target="_blank"
           rel="noopener noreferrer"
           role="menuitem"
-          aria-label="Use our Google Form if you a find bug or issue"
         >
-          Use our Google Form if you find a bug or issue
+          please give us feedback if you find a bug or issue
         </a>{' '}
         whilst using the website.
       </p>
